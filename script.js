2a03c6614f7bf63e448315a73a7016c4980da306
Arie Crespine <crespinearie@gmail.com>
2021-12-06 20:15:06 +0200

    big commit v_1.0

---
+++ script.js
@@ -0,0 +1,22 @@
+// btnEnvoyer = document.querySelector(".form--submit");
+
+// btnEnvoyer.addEventListener("click", () => {
+//   console.log("ta envoyer le form");
+//   var radios = document.getElementsByName("example");
+//   for (var i = 0, length = radios.length; i < length; i++) {
+//     if (radios[i].checked) {
+//       // do whatever you want with the checked radio
+//       console.log(radios[i].getAttribute("value"));
+
+//       // only one radio can be logically checked, don't check the rest
+//       break;
+//     }
+//   }
+// });
+
+// function getAction() {
+
+//     document.myForm.action = "#";
+//     console.log("reussi");
+
+// }
